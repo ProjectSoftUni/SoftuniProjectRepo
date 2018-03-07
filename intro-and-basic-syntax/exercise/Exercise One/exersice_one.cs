811a2040d8587458a068fd6f8056216301486691
StilianKuhev <stiliankushev@gmail.com>
2018-03-07 20:38:37 +0200

    Added the first file.You should do the same for the other files

---
+++ intro-and-basic-syntax/exercise/Exercise One/exersice_one.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exarcise_One
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int firstNumber = int.Parse (Console.ReadLine ());
+			int SecondNumber = int.Parse (Console.ReadLine ());
+			int thirdNumber = int.Parse (Console.ReadLine ());
+			int forthNumber = int.Parse (Console.ReadLine ());
+
+			Console.WriteLine ($"{firstNumber:0000} {SecondNumber:0000} {thirdNumber:0000} {forthNumber:0000}");
+		}
+	}
+}
